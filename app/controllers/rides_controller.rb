c7409361fcecc60bb97f168f2e4529d5778dcfc9
Savannah C <sgcorrell@gmail.com>
2019-08-29 17:14:26 +0000

    Automatically backed up by Learn

--- app/controllers/rides_controller.rb
+++ app/controllers/rides_controller.rb
@@ -1,15 +1,9 @@
 class RidesController < ApplicationController
   
   def create
-    @ride = Ride.new(ride_params)
+    @ride = Ride.new(user_id: current_user.id, attraction_id: params[:attraction_id])
     flash[:notice] = @ride.take_ride
     redirect_to user_path(current_user)
-  end
-
-  private
-
-  def ride_params
-    params.require(:ride).permit(:user_id, :attraction_id)
   end
   
 end
